93531ede0178d8d6e8c43f36d6597f4bb79c4bad
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2018-02-28 07:13:48 +0100

    transformation of normals

--- include/gbMath/Transform2.hpp
+++ include/gbMath/Transform2.hpp
@@ -9,6 +9,7 @@
 
 #include <gbMath/config.hpp>
 
+#include <gbMath/Matrix2.hpp>
 #include <gbMath/Matrix3.hpp>
 #include <gbMath/NumberTypeTraits.hpp>
 #include <gbMath/Vector2.hpp>
@@ -17,6 +18,26 @@
 
 namespace GHULBUS_MATH_NAMESPACE
 {
+/** The reciprocal of a Transform2.
+ * Normals and other reciprocal vectors can only be transformed through a reciprocal transform, which is equal to
+ * the inverse of the original transform.
+ * Unlike transforms, reciprocal transforms are multiplied from the right (alternatively, we would have to transpose
+ * before multiplying from the left).
+ * Since normals are points at infinity, we omit the translation portion and only store a Matrix2 to speed up
+ * calculation of the inverse.
+ * The reciprocal transformation matrix does not preserve scale. Reciprocally transformed vectors can be re-normalized
+ * by dividing by the determinant of the original transformation matrix.
+ */
+template<typename T>
+class TransformReciprocal2
+{
+public:
+    Matrix2<T> m;
+};
+
+struct assume_orthogonal_t {};
+//inline assume_orthogonal_t assume_orthogonal;
+
 /** A homogeneous 2D transformation.
  */
 template<typename T>
@@ -36,6 +57,31 @@ public:
            n21, n22, n23,
            n31, n32, n33)
     {}
+
+    /** Retrieve the reciprocal of the current transform.
+     * Normals and other reciprocal vectors are transformed by multiplying the inverse transpose of the original
+     * transformation. We follow Lengyel's suggestion here by using the adjugate to properly mirror normals if the
+     * winding order changed and instead of transposing, multiply it from the right.
+     */
+    TransformReciprocal2<T> reciprocal()
+    {
+        Matrix2<T> const upper_left(m.m11, m.m12,
+                                    m.m21, m.m22);
+        return TransformReciprocal2<T>{ adjugate(upper_left) };
+    }
+
+    /** Retrieve the reciprocal under the assumption that the transform is orthogonal.
+     * For orthogonal matrices, the inverse is equal to the transpose. Since transposing is way less computationally
+     * expensive than proper inverting, use this function as a more efficient means of obtaining a reciprocal
+     * if you know beforehand that the transform is in fact orthogonal.
+     * @pre this->m * transpose(this->m) ~= identity3()
+     */
+    TransformReciprocal2<T> reciprocal(assume_orthogonal_t)
+    {
+        Matrix2<T> const upper_left(m.m11, m.m12,
+                                    m.m21, m.m22);
+        return TransformReciprocal2<T>{ transpose(upper_left) };
+    }
 };
 
 template<typename T, typename VectorTag_T>
@@ -54,6 +100,15 @@ operator*(Transform2<T> const& t, Vector2Impl<T, VectorTag_T> const& p)
     Matrix3<T> const& m = t.m;
     return Vector2Impl<T, VectorTag_T>(m.m11*p.x + m.m12*p.y + m.m13,
                                        m.m21*p.x + m.m22*p.y + m.m23);
+}
+
+template<typename T, typename VectorTag_T>
+inline std::enable_if_t<VectorTraits::IsReciprocal<VectorTag_T>::value && !VectorTraits::IsFinitePoint<VectorTag_T>::value, Vector2Impl<T, VectorTag_T>>
+operator*(Vector2Impl<T, VectorTag_T> const& n, TransformReciprocal2<T> const& rt)
+{
+    Matrix2<T> const& m = rt.m;
+    return Vector2Impl<T, VectorTag_T>(n.x*m.m11 + n.y*m.m21,
+                                       n.x*m.m12 + n.y*m.m22);
 }
 
 template<typename T>
--- test/TestTransform2.cpp
+++ test/TestTransform2.cpp
@@ -5,7 +5,9 @@
 
 TEST_CASE("Transform2")
 {
+    using GHULBUS_MATH_NAMESPACE::Matrix2;
     using GHULBUS_MATH_NAMESPACE::Matrix3;
+    using GHULBUS_MATH_NAMESPACE::Normal2;
     using GHULBUS_MATH_NAMESPACE::Point2;
     using GHULBUS_MATH_NAMESPACE::Vector2;
     using GHULBUS_MATH_NAMESPACE::Transform2;
@@ -44,11 +46,40 @@ TEST_CASE("Transform2")
                                 75.f,  80.f,  95.f)*p == Point2<float>(20.f, -40.f));
     }
 
+    SECTION("Reciprocal transformation")
+    {
+        Transform2<float> t(1.f, 0.f, 0.f,
+                            0.f, 2.f, 0.f,
+                            0.f, 0.f, 1.f);
+        CHECK(determinant(t.m) == 2.f);
+        CHECK(t.reciprocal().m == Matrix2<float>(2.f, 0.f,
+                                                 0.f, 1.f));
+        CHECK(determinant(t.reciprocal().m) == 2.f);
+    }
+
+    SECTION("Reciprocal transformation of orthogonal")
+    {
+        Transform2<float> t(0.f, 1.f, 0.f,
+                           -1.f, 0.f, 0.f,
+                            0.f, 0.f, 1.f);
+        REQUIRE(determinant(t.m) == 1.f);
+        GHULBUS_MATH_NAMESPACE::assume_orthogonal_t orth{};
+        CHECK(t.reciprocal(orth).m == Matrix2<float>(0.f,-1.f,
+                                                     1.f, 0.f));
+    }
+
     SECTION("Scale matrix")
     {
         auto scale = GHULBUS_MATH_NAMESPACE::make_scale(4.f, 2.f);
         CHECK(scale.m == Matrix3<float>(4.f, 0.f, 0.f,
                                         0.f, 2.f, 0.f,
                                         0.f, 0.f, 1.f));
+    }
+
+    SECTION("Normal transformation")
+    {
+        Normal2<float> n(1.f, 1.f);
+        Transform2<float> t = GHULBUS_MATH_NAMESPACE::make_scale(2.f, 1.f);
+        CHECK(n*t.reciprocal() == Normal2<float>(1.f, 2.f));
     }
 }
