b2ad1db7b4fec25b14353eb8e70f58d58911179a
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2018-03-03 11:25:49 +0100

    line3

---
+++ include/gbMath/Line3.hpp
@@ -0,0 +1,91 @@
+#ifndef GHULBUS_LIBRARY_INCLUDE_GUARD_MATH_LINE3_HPP
+#define GHULBUS_LIBRARY_INCLUDE_GUARD_MATH_LINE3_HPP
+
+/** @file
+*
+* @brief 3D Line.
+* @author Andreas Weis (der_ghulbus@ghulbus-inc.de)
+*/
+
+#include <gbMath/config.hpp>
+
+#include <gbMath/Vector3.hpp>
+
+namespace GHULBUS_MATH_NAMESPACE
+{
+template<typename T>
+class Line3 {
+public:
+    Point3<T> p;
+    Vector3<T> v;
+public:
+    Line3() = default;
+    Line3(Line3<T> const&) = default;
+    Line3<T>& operator=(Line3<T> const&) = default;
+
+    Line3(Point3<T> const& np, Vector3<T> const& nv)
+        :p(np), v(nv)
+    {}
+
+    Line3(Point3<T> const& p1, Point3<T> const& p2)
+        :p(p1), v(p2 - p1)
+    {}
+};
+
+template<typename T>
+inline std::enable_if_t<std::is_floating_point<T>::value, Line3<T>> normalized(Line3<T> const& l)
+{
+    return Line3<T>(l.p, normalized(l.v));
+}
+
+template<typename T>
+inline T distance_squared_unit(Line3<T> const& l, Point3<T> const& p)
+{
+    auto const a = cross(p - l.p, l.v);
+    return dot(a, a);
+}
+
+template<typename T>
+inline T distance_squared(Line3<T> const& l, Point3<T> const& p)
+{
+    return distance_squared_unit(l, p) / dot(l.v, l.v);
+}
+
+template<typename T>
+inline T distance(Line3<T> const& l, Point3<T> const& p)
+{
+    return std::sqrt(static_cast<double>(distance_squared(l, p)));
+}
+
+template<>
+inline float distance(Line3<float> const& l, Point3<float> const& p)
+{
+    return std::sqrt(distance_squared(l, p));
+}
+
+template<>
+inline long double distance(Line3<long double> const& l, Point3<long double> const& p)
+{
+    return std::sqrt(distance_squared(l, p));
+}
+
+template<typename T>
+inline T distance_unit(Line3<T> const& l, Point3<T> const& p)
+{
+    return std::sqrt(static_cast<double>(distance_squared_unit(l, p)));
+}
+
+template<>
+inline float distance_unit(Line3<float> const& l, Point3<float> const& p)
+{
+    return std::sqrt(distance_squared_unit(l, p));
+}
+
+template<>
+inline long double distance_unit(Line3<long double> const& l, Point3<long double> const& p)
+{
+    return std::sqrt(distance_squared_unit(l, p));
+}
+
+}
+#endif
---
+++ test/TestLine3.cpp
@@ -0,0 +1,85 @@
+#include <gbMath/Line3.hpp>
+#include <gbMath/VectorIO.hpp>
+
+#include <catch.hpp>
+
+namespace {
+    template<typename T> struct Test_PointLineDistance {
+        void operator()()
+        {
+            using GHULBUS_MATH_NAMESPACE::Line3;
+            using GHULBUS_MATH_NAMESPACE::Point3;
+            using GHULBUS_MATH_NAMESPACE::Vector3;
+
+            Line3<T> const l0(Point3<T>(0.f, 0.f, 0.f), Vector3<T>(1.f, 1.f, 1.f));
+            auto const l0_norm = normalized(l0);
+            Line3<T> const l1(Point3<T>(-4.f, 2.5f, 4.5f), Vector3<T>(2.f, -2.5f, 2.5f));
+            auto const l1_norm = normalized(l1);
+
+            Point3<T> const p0(5.f, -2.f, 1.f);
+            CHECK(distance_squared(l0, p0) == Approx(24.f + (2.f/3.f)));
+            CHECK(distance(l0, p0) == Approx(std::sqrt(24.f + (2.f/3.f))));
+            CHECK(distance_squared_unit(l0_norm, p0) == Approx(24.f + (2.f/3.f)));
+            CHECK(distance_unit(l0_norm, p0) == Approx(std::sqrt(24.f + (2.f/3.f))));
+
+            CHECK(distance_squared(l1, p0) == Approx(88.f + (1.f/33.f)));
+            CHECK(distance(l1, p0) == Approx(std::sqrt(88.f + (1.f/33.f))));
+            CHECK(distance_squared_unit(l1_norm, p0) == Approx(88.f + (1.f/33.f)));
+            CHECK(distance_unit(l1_norm, p0) == Approx(std::sqrt(88.f + (1.f/33.f))));
+
+            Point3<T> const p1(-4.f, -1.f, 16.f);
+            CHECK(distance_squared(l0, p1) == Approx(232.f + (2.f/3.f)));
+            CHECK(distance(l0, p1) == Approx(std::sqrt(232.f + (2.f/3.f))));
+            CHECK(distance_squared_unit(l0_norm, p1) == Approx(232.f + (2.f/3.f)));
+            CHECK(distance_unit(l0_norm, p1) == Approx(std::sqrt(232.f + (2.f/3.f))));
+
+            CHECK(distance_squared(l1, p1) == Approx(59.f + (3.f/11.f)));
+            CHECK(distance(l1, p1) == Approx(std::sqrt(59.f + (3.f/11.f))));
+            CHECK(distance_squared_unit(l1_norm, p1) == Approx(59.f + (3.f/11.f)));
+            CHECK(distance_unit(l1_norm, p1) == Approx(std::sqrt(59.f + (3.f/11.f))));
+        }
+    };
+}
+
+TEST_CASE("Line3")
+{
+    using GHULBUS_MATH_NAMESPACE::Line3;
+    using GHULBUS_MATH_NAMESPACE::Point3;
+    using GHULBUS_MATH_NAMESPACE::Vector3;
+
+    SECTION("Zero Initialization")
+    {
+        Line3<float> l{};
+        CHECK(l.p == Point3<float>(0.f, 0.f, 0.f));
+        CHECK(l.v == Vector3<float>(0.f, 0.f, 0.f));
+    }
+
+    SECTION("Construction from point and vector")
+    {
+        Line3<float> l(Point3<float>(1.f, 2.f, 3.f), Vector3<float>(-4.f, 5.f, -6.f));
+        CHECK(l.p == Point3<float>(1.f, 2.f, 3.f));
+        CHECK(l.v == Vector3<float>(-4.f, 5.f, -6.f));
+    }
+
+    SECTION("Construction from two points")
+    {
+        Line3<float> l(Point3<float>(1.f, 2.f, 3.f), Point3<float>(-4.f, 5.f, -6.f));
+        CHECK(l.p == Point3<float>(1.f, 2.f, 3.f));
+        CHECK(l.v == Vector3<float>(-5.f, 3.f, -9.f));
+    }
+
+    SECTION("Normalization")
+    {
+        Line3<float> l = normalized(Line3<float>(Point3<float>(1.f, 2.f, 3.f), Point3<float>(-4.f, 5.f, -6.f)));
+        CHECK(l.p == Point3<float>(1.f, 2.f, 3.f));
+        CHECK(l.v == Vector3<float>(-5.f/std::sqrt(115.f), 3.f/std::sqrt(115.f), -9.f/std::sqrt(115.f)));
+        CHECK(length(l.v) == Approx(1.f));
+    }
+
+    SECTION("Distance Point-Line")
+    {
+        Test_PointLineDistance<float>()();
+        Test_PointLineDistance<double>()();
+        Test_PointLineDistance<long double>()();
+    }
+}
