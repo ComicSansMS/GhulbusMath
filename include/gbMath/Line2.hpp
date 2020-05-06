9cc60bbad360e8b421b18e8b443fb85a4bda7577
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2020-05-06 18:27:17 +0200

    2d line-line intersection

--- include/gbMath/Line2.hpp
+++ include/gbMath/Line2.hpp
@@ -31,9 +31,10 @@ public:
         :p(p1), v(p2 - p1)
     {}
 
-    Point2<T> evaluate_at_parameter(T t) const
+    template<typename U = T>
+    Point2<U> evaluate_at_parameter(U t) const
     {
-        return p + v * t;
+        return Point2<U>(p) + Vector2<U>(v) * t;
     }
 };
 
@@ -67,6 +68,91 @@ template<typename T>
 inline T distance_unit(Line2<T> const& l, Point2<T> const& p)
 {
     return length(reject_unit((p - l.p), l.v));
+}
+
+/** Represents the result of a line-line intersection along with a scaling factor.
+ * Use this to model intersections with fractional parameters using integral types.
+ * The intersection point for two lines `l1` and `l2` can be retrieved from a ScaledLine2Intersection `sli` by calling
+ * `l1.evaluate_at_parameter(sli.evaluate_t1())`
+ * or
+ * `l2.evaluate_at_parameter(sli.evaluate_t2())`.
+ * @see intersect_scaled()
+ */
+template<typename T>
+struct ScaledLine2Intersection {
+    T t1;                       ///< The scaled parameter at which the first line reaches the intersection point.
+    T t2;                       ///< The scaled parameter at which the first line reaches the intersection point.
+    T inverse_scale_factor;     ///< The scale factor by which t1 and t2 need to be divided to retrieve the
+                                ///<  line parameters. For parallel lines, this will be 0.
+
+    ScaledLine2Intersection() = default;
+    ScaledLine2Intersection(ScaledLine2Intersection const&) = default;
+    ScaledLine2Intersection& operator=(ScaledLine2Intersection const&) = default;
+
+    ScaledLine2Intersection(T n_t1, T n_t2, T n_inverse_scale_factor)
+        :t1(n_t1), t2(n_t2), inverse_scale_factor(n_inverse_scale_factor)
+    {}
+
+    /**
+     * @pre inverse_scale_factor != 0
+     */
+    template<typename U = T>
+    U evaluate_t1() const {
+        return static_cast<U>(t1) / static_cast<U>(inverse_scale_factor);
+    }
+
+    /**
+    * @pre inverse_scale_factor != 0
+    */
+    template<typename U = T>
+    U evaluate_t2() const {
+        return static_cast<U>(t2) / static_cast<U>(inverse_scale_factor);
+    }
+};
+
+/** Computes the intersection of two lines @p l1 and @p l2.
+ * This function computes the line parameters for the intersection as a ScaledLine2Intersection, which is lossless
+ * for integral types. If the lines are non-parallel, the `inverse_scale_factor` of the return value will be `!= 0`.
+ * If the lines coincide, all fields of the return value will be `0`.
+ * @code{.c}
+ * Line2<int> l1;
+ * Line2<int> l2;
+ * // [...]
+ * ScaledLine2Intersection<int> const sli = intersect_scaled(l1, l2);
+ * if (sli.inverse_scale_factor != 0) {
+ *   Point2<float> intersection_point = l1.evaluate_at_parameter(sli.evaluate_t1<float>());
+ *   // [...]
+ * }
+ * @endcode
+ */
+template<typename T>
+[[nodiscard]] inline ScaledLine2Intersection<T> intersect_scaled(Line2<T> const& l1, Line2<T> const& l2)
+{
+    // The intersection point is obtained by solving the linear system
+    //  [ l1.v -l2.v ] [ t ] = [ l2.p - l1.p ]
+    // for t = (t1, t2), the vector of line parameters.
+    // The linear system is solved by
+    // [ t ] = [ l1.v -l2.v ]^(-1) [ l2.p - l1.p ]
+    T const det = (l1.v.y * l2.v.x) - (l1.v.x * l2.v.y);
+    Vector2<T> const dp = l2.p - l1.p;
+    T const t1_scaled = dot(dp, Vector2<T>(-l2.v.y, l2.v.x));
+    T const t2_scaled = dot(dp, Vector2<T>(-l1.v.y, l1.v.x));
+    return ScaledLine2Intersection(t1_scaled, t2_scaled, det);
+}
+
+/** Computes the intersection point of two lines @p l1 and @p l2.
+ * @return The unique intersection point, if it exists.
+ *         If the two lines are parallel, the point coordinates will be infinity.
+ *         If the two lines coincide, the point coordinates will be NaN.
+ */
+template<typename T>
+[[nodiscard]] inline Point2<T> intersect_p(Line2<T> const& l1, Line2<T> const& l2)
+{
+    // We only need to solve one of the ts to get the intersection point.
+    T const det = (l1.v.x * l2.v.y) - (l1.v.y * l2.v.x);
+    Vector2<T> const dp = l2.p - l1.p;
+    T const t1_scaled = dot(dp, Vector2<T>(l2.v.y, -l2.v.x));
+    return l1.evaluate_at_parameter(t1_scaled / det);
 }
 }
 #endif
--- test/TestLine2.cpp
+++ test/TestLine2.cpp
@@ -1,6 +1,9 @@
 #include <gbMath/Line2.hpp>
 #include <gbMath/VectorIO.hpp>
 
+#include <cmath>
+#include <limits>
+
 #include <catch.hpp>
 
 TEST_CASE("Line2")
@@ -32,11 +35,11 @@ TEST_CASE("Line2")
 
     SECTION("Evaluate line equation at a specific parameter")
     {
-        Line2<float> l(Point2<float>(1.f, 2.f), Point2<float>(-3.f, 5.f));
-        CHECK(l.evaluate_at_parameter(0.f) == l.p);
-        CHECK(l.evaluate_at_parameter(1.f) == l.p + l.v);
-        CHECK(l.evaluate_at_parameter(0.5f) == l.p + (l.v / 2.f));
-        CHECK(l.evaluate_at_parameter(2.5f) == l.p + (l.v * 2.5f));
+        Line2<int> l(Point2<int>(1, 2), Point2<int>(-3, 5));
+        CHECK(l.evaluate_at_parameter(0) == l.p);
+        CHECK(l.evaluate_at_parameter(1) == l.p + l.v);
+        CHECK(l.evaluate_at_parameter(0.5f) == Point2<float>(l.p) + (Vector2<float>(l.v) / 2.f));
+        CHECK(l.evaluate_at_parameter(2.5) == Point2<double>(l.p) + (Vector2<double>(l.v) * 2.5));
     }
 
     SECTION("Normalization")
@@ -75,5 +78,147 @@ TEST_CASE("Line2")
         CHECK(distance(l1, p1) == std::sqrt(13.225f));
         CHECK(distance_squared_unit(l1_norm, p1) == 13.225f);
         CHECK(distance_unit(l1_norm, p1) == std::sqrt(13.225f));
+    }
+
+    SECTION("Scaled Line2 Intersection Zero Initialization")
+    {
+        using GHULBUS_MATH_NAMESPACE::ScaledLine2Intersection;
+        ScaledLine2Intersection<int> sli{};
+        CHECK(sli.t1 == 0);
+        CHECK(sli.t2 == 0);
+        CHECK(sli.inverse_scale_factor == 0);
+    }
+
+    SECTION("Scaled Line2 Intersection Construction From Values")
+    {
+        using GHULBUS_MATH_NAMESPACE::ScaledLine2Intersection;
+        ScaledLine2Intersection<int> sli(11, 22, 33);
+        CHECK(sli.t1 == 11);
+        CHECK(sli.t2 == 22);
+        CHECK(sli.inverse_scale_factor == 33);
+    }
+
+    SECTION("Scaled Line2 Intersection Copy Construction")
+    {
+        using GHULBUS_MATH_NAMESPACE::ScaledLine2Intersection;
+        ScaledLine2Intersection<int> sli(11, 22, 33);
+        ScaledLine2Intersection<int> sli_copy(sli);
+        CHECK(sli_copy.t1 == 11);
+        CHECK(sli_copy.t2 == 22);
+        CHECK(sli_copy.inverse_scale_factor == 33);
+    }
+
+    SECTION("Scaled Line2 Intersection Copy Assignment")
+    {
+        using GHULBUS_MATH_NAMESPACE::ScaledLine2Intersection;
+        ScaledLine2Intersection<int> sli(11, 22, 33);
+        ScaledLine2Intersection<int> sli2(5, 6, 7);
+        CHECK(sli.t1 == 11);
+        CHECK(sli.t2 == 22);
+        CHECK(sli.inverse_scale_factor == 33);
+        sli = sli2;
+        CHECK(sli.t1 == 5);
+        CHECK(sli.t2 == 6);
+        CHECK(sli.inverse_scale_factor == 7);
+    }
+
+    SECTION("Scaled Line2 Intersection Evaluation")
+    {
+        using GHULBUS_MATH_NAMESPACE::ScaledLine2Intersection;
+        ScaledLine2Intersection<int> sli(1, 2, 4);
+        CHECK(sli.evaluate_t1<float>() == 0.25f);
+        CHECK(sli.evaluate_t2<float>() == 0.5f);
+
+        ScaledLine2Intersection<int> sli_weird_values(0, 1, 0);
+        CHECK(std::isnan(sli_weird_values.evaluate_t1<float>()));
+        CHECK(sli_weird_values.evaluate_t2<float>() == std::numeric_limits<float>::infinity());
+    }
+
+    SECTION("Scaled Line-Line Intersection")
+    {
+        {
+            Line2<int> l1(Point2<int>(0, 0), Vector2<int>(1, 1));
+            Line2<int> l2(Point2<int>(1, 0), Vector2<int>(-1, 1));
+            auto const sli = intersect_scaled(l1, l2);
+            CHECK(sli.t1 == -1);
+            CHECK(sli.t2 == -1);
+            CHECK(sli.inverse_scale_factor == -2);
+            auto p1 = l1.evaluate_at_parameter(sli.evaluate_t1<float>());
+            auto p2 = l2.evaluate_at_parameter(sli.evaluate_t2<float>());
+            CHECK(l1.evaluate_at_parameter(sli.evaluate_t1<float>()) ==
+                  l2.evaluate_at_parameter(sli.evaluate_t2<float>()));
+        }
+        {
+            Line2<int> l1(Point2<int>(0, 0), Vector2<int>(10, 1));
+            Line2<int> l2(Point2<int>(50, 0), Vector2<int>(0, -2));
+            auto const sli = intersect_scaled(l1, l2);
+            CHECK(sli.t1 == 100);
+            CHECK(sli.t2 == -50);
+            CHECK(sli.inverse_scale_factor == 20);
+            CHECK(l1.evaluate_at_parameter(sli.evaluate_t1<float>()) ==
+                  l2.evaluate_at_parameter(sli.evaluate_t2<float>()));
+        }
+        {
+            // parallel lines
+            Line2<int> l1(Point2<int>(0, 0), Vector2<int>(10, 15));
+            Line2<int> l2(Point2<int>(50, 0), Vector2<int>(20, 30));
+            auto const sli = intersect_scaled(l1, l2);
+            CHECK(sli.inverse_scale_factor == 0);
+        }
+        {
+            // identical lines (same parameters)
+            Line2<int> l(Point2<int>(50, 0), Vector2<int>(20, 30));
+            auto const sli = intersect_scaled(l, l);
+            CHECK(sli.t1 == 0);
+            CHECK(sli.t2 == 0);
+            CHECK(sli.inverse_scale_factor == 0);
+        }
+        {
+            // identical lines (different parameters)
+            Line2<int> l1(Point2<int>(-10, -90), Vector2<int>(20, 30));
+            Line2<int> l2(Point2<int>(50, 0), Vector2<int>(20, 30));
+            auto const sli = intersect_scaled(l1, l2);
+            CHECK(sli.t1 == 0);
+            CHECK(sli.t2 == 0);
+            CHECK(sli.inverse_scale_factor == 0);
+        }
+    }
+
+    SECTION("Line-Line Intersection Point")
+    {
+        {
+            Line2<float> l1(Point2<float>(0, 0), Vector2<float>(1, 1));
+            Line2<float> l2(Point2<float>(1, 0), Vector2<float>(-1, 1));
+            auto const p = intersect_p(l1, l2);
+            CHECK(p == Point2<float>(0.5f, 0.5f));
+        }
+        {
+            Line2<float> l1(Point2<float>(0, 0), Vector2<float>(10, 1));
+            Line2<float> l2(Point2<float>(50, 0), Vector2<float>(0, -2));
+            auto const p = intersect_p(l1, l2);
+            CHECK(p == Point2<float>(50.f, 5.f));
+        }
+        {
+            // parallel lines
+            Line2<float> l1(Point2<float>(0, 0), Vector2<float>(10, 15));
+            Line2<float> l2(Point2<float>(50, 0), Vector2<float>(20, 30));
+            auto const p = intersect_p(l1, l2);
+            CHECK(p == Point2<float>(std::numeric_limits<float>::infinity(), std::numeric_limits<float>::infinity()));
+        }
+        {
+            // identical lines (same parameters)
+            Line2<float> l(Point2<float>(50, 0), Vector2<float>(20, 30));
+            auto const p = intersect_p(l, l);
+            CHECK(std::isnan(p.x));
+            CHECK(std::isnan(p.y));
+        }
+        {
+            // identical lines (different parameters)
+            Line2<float> l1(Point2<float>(-10, -90), Vector2<float>(20, 30));
+            Line2<float> l2(Point2<float>(50, 0), Vector2<float>(20, 30));
+            auto const p = intersect_p(l1, l2);
+            CHECK(std::isnan(p.x));
+            CHECK(std::isnan(p.y));
+        }
     }
 }
