cf175f3a5230fb40f52075ff03435a8dd4e61fd7
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2020-05-08 20:32:10 +0200

    cleaned up includes

--- include/gbMath/Circle2.hpp
+++ include/gbMath/Circle2.hpp
@@ -13,15 +13,7 @@
 #include <gbMath/NumberTypeTraits.hpp>
 #include <gbMath/Vector2.hpp>
 
-#include <algorithm>
 #include <cmath>
-#include <limits>
-#ifndef  __APPLE__
-#include <optional>
-#else
-#include <experimental/optional>
-namespace std { using std::experimental::optional; using std::experimental::nullopt; }
-#endif
 #include <type_traits>
 
 namespace GHULBUS_MATH_NAMESPACE
