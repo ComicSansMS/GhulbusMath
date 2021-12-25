6eb424c19eee4a9d3070da944bcb864f587edae6
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2021-12-25 10:00:45 +0100

    line3 transformation

---
+++ include/gbMath/TransformLine3.hpp
@@ -0,0 +1,28 @@
+#ifndef GHULBUS_LIBRARY_INCLUDE_GUARD_MATH_TRANSFORMLINE3_HPP
+#define GHULBUS_LIBRARY_INCLUDE_GUARD_MATH_TRANSFORMLINE3_HPP
+
+/** @file
+ *
+ * @brief Transformation of 3D Lines.
+ * @author Andreas Weis (der_ghulbus@ghulbus-inc.de)
+ */
+
+#include <gbMath/config.hpp>
+
+#include <gbMath/Line3.hpp>
+#include <gbMath/Transform3.hpp>
+
+namespace GHULBUS_MATH_NAMESPACE
+{
+
+template<typename T>
+inline Line3<T> operator*(Transform3<T> const& transform, Line3<T> const& line) {
+    return Line3<T>(
+        transform * line.p,
+        transform * line.v
+    );
+}
+
+}
+
+#endif
---
+++ test/TestTransformLine3.cpp
@@ -0,0 +1,42 @@
+#include <gbMath/TransformLine3.hpp>
+#include <gbMath/VectorIO.hpp>
+
+#include <catch.hpp>
+
+TEST_CASE("TransformLine3")
+{
+    using GHULBUS_MATH_NAMESPACE::Line3;
+    using GHULBUS_MATH_NAMESPACE::Point3;
+    using GHULBUS_MATH_NAMESPACE::Vector3;
+    using GHULBUS_MATH_NAMESPACE::Transform3;
+
+    Line3<float> const source_line(Point3<float>(1.f, 2.f, 3.f), Vector3<float>(1.f, 0.f, 0.f));
+
+    SECTION("Translation of Line")
+    {
+        Transform3<float> translation = GHULBUS_MATH_NAMESPACE::make_translation(4.f, 5.f, 6.f);
+        Line3<float> const l = translation * source_line;
+        CHECK(l.p == Point3<float>(5.f, 7.f, 9.f));
+        CHECK(l.v == source_line.v);
+    }
+
+    SECTION("Scaling of Line")
+    {
+        Transform3<float> scale = GHULBUS_MATH_NAMESPACE::make_scale(4.f, 5.f, 6.f);
+        Line3<float> const l = scale * source_line;
+        CHECK(l.p == Point3<float>(4.f, 10.f, 18.f));
+        CHECK(l.v == Vector3<float>(4.f, 0.f, 0.f));
+    }
+
+    SECTION("Rotation of Line")
+    {
+        Transform3<float> rotation = GHULBUS_MATH_NAMESPACE::make_rotation_z(GHULBUS_MATH_NAMESPACE::traits::Pi<float>::value / 2.f);
+        Line3<float> const l = rotation * source_line;
+        CHECK(l.p.x == Approx(-2.f));
+        CHECK(l.p.y == Approx(1.f));
+        CHECK(l.p.z == 3.f);
+        CHECK(l.v.x == Approx(0.f).margin(1e-6f));
+        CHECK(l.v.y == Approx(1.f));
+        CHECK(l.v.z == 0.f);
+    }
+}
