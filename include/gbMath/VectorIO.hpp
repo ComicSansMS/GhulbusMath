a1ff09a7ab267b40c52686c3f19f170b17a8432c
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2023-09-03 17:02:33 +0200

    formatting fallback for older std library versions

--- include/gbMath/VectorIO.hpp
+++ include/gbMath/VectorIO.hpp
@@ -3,7 +3,7 @@
 
 /** @file
  *
- * @brief ostream inserter functions for Vector types.
+ * @brief Formatters and ostream inserter functions for Vector types.
  * @author Andreas Weis (der_ghulbus@ghulbus-inc.de)
  */
 
@@ -12,8 +12,10 @@
 #include <gbMath/Vector3.hpp>
 #include <gbMath/Vector4.hpp>
 
-#include <format>
 #include <ostream>
+
+#ifdef __cpp_lib_format
+#include <format>
 
 template<class T, typename VectorTag_T, class CharT>
 struct std::formatter<GHULBUS_MATH_NAMESPACE::Vector2Impl<T, VectorTag_T>, CharT>
@@ -121,5 +123,95 @@ inline std::ostream& operator<<(std::ostream& os, Vector4<T> const& rhs)
     return os << std::format("{}", rhs);
 }
 }
+
+#else
+
+namespace GHULBUS_MATH_NAMESPACE
+{
+    template<typename T, typename VectorTag_T>
+    inline std::ostream& operator<<(std::ostream& os, Vector2Impl<T, VectorTag_T> const& rhs)
+    {
+        os << '[' << rhs.x << ' ' << rhs.y << ']';
+        return os;
+    }
+
+    template<typename VectorTag_T>
+    inline std::ostream& operator<<(std::ostream& os, Vector2Impl<char, VectorTag_T> const& rhs)
+    {
+        os << '[' << static_cast<int>(rhs.x) << ' ' << static_cast<int>(rhs.y) << ']';
+        return os;
+    }
+
+    template<typename VectorTag_T>
+    inline std::ostream& operator<<(std::ostream& os, Vector2Impl<signed char, VectorTag_T> const& rhs)
+    {
+        os << '[' << static_cast<int>(rhs.x) << ' ' << static_cast<int>(rhs.y) << ']';
+        return os;
+    }
+
+    template<typename VectorTag_T>
+    inline std::ostream& operator<<(std::ostream& os, Vector2Impl<unsigned char, VectorTag_T> const& rhs)
+    {
+        os << '[' << static_cast<int>(rhs.x) << ' ' << static_cast<int>(rhs.y) << ']';
+        return os;
+    }
+
+    template<typename T, typename VectorTag_T>
+    inline std::ostream& operator<<(std::ostream& os, Vector3Impl<T, VectorTag_T> const& rhs)
+    {
+        os << '[' << rhs.x << ' ' << rhs.y << ' ' << rhs.z << ']';
+        return os;
+    }
+
+    template<typename VectorTag_T>
+    inline std::ostream& operator<<(std::ostream& os, Vector3Impl<char, VectorTag_T> const& rhs)
+    {
+        os << '[' << static_cast<int>(rhs.x) << ' ' << static_cast<int>(rhs.y) << ' ' << static_cast<int>(rhs.z) << ']';
+        return os;
+    }
+
+    template<typename VectorTag_T>
+    inline std::ostream& operator<<(std::ostream& os, Vector3Impl<signed char, VectorTag_T> const& rhs)
+    {
+        os << '[' << static_cast<int>(rhs.x) << ' ' << static_cast<int>(rhs.y) << ' ' << static_cast<int>(rhs.z) << ']';
+        return os;
+    }
+
+    template<typename VectorTag_T>
+    inline std::ostream& operator<<(std::ostream& os, Vector3Impl<unsigned char, VectorTag_T> const& rhs)
+    {
+        os << '[' << static_cast<int>(rhs.x) << ' ' << static_cast<int>(rhs.y) << ' ' << static_cast<int>(rhs.z) << ']';
+        return os;
+    }
+
+    template<typename T>
+    inline std::ostream& operator<<(std::ostream& os, Vector4<T> const& rhs)
+    {
+        os << '[' << rhs.x << ' ' << rhs.y << ' ' << rhs.z << ' ' << rhs.w << ']';
+        return os;
+    }
+
+    inline std::ostream& operator<<(std::ostream& os, Vector4<char> const& rhs)
+    {
+        os << '[' << static_cast<int>(rhs.x) << ' ' << static_cast<int>(rhs.y) << ' ' << static_cast<int>(rhs.z) << ' ' << static_cast<int>(rhs.w) << ']';
+        return os;
+    }
+
+
+    inline std::ostream& operator<<(std::ostream& os, Vector4<signed char> const& rhs)
+    {
+        os << '[' << static_cast<int>(rhs.x) << ' ' << static_cast<int>(rhs.y) << ' ' << static_cast<int>(rhs.z) << ' ' << static_cast<int>(rhs.w) << ']';
+        return os;
+    }
+
+
+    inline std::ostream& operator<<(std::ostream& os, Vector4<unsigned char> const& rhs)
+    {
+        os << '[' << static_cast<int>(rhs.x) << ' ' << static_cast<int>(rhs.y) << ' ' << static_cast<int>(rhs.z) << ' ' << static_cast<int>(rhs.w) << ']';
+        return os;
+    }
+}
+
+#endif
 
 #endif
